2fc7ab5063a5ba74171952a77e3a77dfa2f1c256
Paweł Paprota <ppawel@fastmail.fm>
2012-10-26 17:05:55 +0200

    Added a helper script for downloading OSC files.

---
+++ scripts/download_osc_files_for_year.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+YEAR=$1
+
+if [ "$YEAR" = "" ]; then
+	echo "Year argument missing!"
+	exit 1
+fi
+
+wget -r -nH --cut-dirs=2 -A.gz http://planet.openstreetmap.org/cc-by-sa/history/$1
