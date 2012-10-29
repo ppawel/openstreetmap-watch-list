72d1cd94072713566abc0d64790500448c203746
Paweł Paprota <ppawel@fastmail.fm>
2012-10-29 23:06:46 +0100

    Cleanup, added tile API and initial impl on the server side.

--- owl_viewer/app/controllers/changeset_controller.rb
+++ owl_viewer/app/controllers/changeset_controller.rb
@@ -1,21 +1,14 @@
 class ChangesetController < ApplicationController
   def changesets
-    bbox = params['bbox'].split(/,/).map { |x| x.to_f }
-    area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
-
-    #if (area < max_area)
-      @changesets = find_changesets_by_bbox(bbox)
-    #end
-
+    bbox = params[:bbox].split(/,/).map { |x| x.to_f }
+    @changesets = find_changesets_by_bbox(bbox)
     render :template => "changeset/changesets.#{params[:format]}", :layout => false
   end
 
-  def dailymap
-    @title = "owl_viewer | Map of changes over the past day"
-  end
-
-  def weeklymap
-    @title = "owl_viewer | Map of changes over the past week"
+  def tile
+    bbox = xyz_to_bbox(params[:x].to_i, params[:y].to_i, params[:zoom].to_i)
+    @changesets = find_changesets_by_bbox(bbox)
+    render :template => "changeset/changesets.#{params[:format]}", :layout => false
   end
 
   def map
@@ -30,5 +23,19 @@ private
       :joins => :user,
       :limit => 100,
       :order => 'created_at DESC')
+  end
+
+  def xyz_to_bbox(x, y, z)
+    tl = xyz_to_latlon(x, y, z)
+    br = xyz_to_latlon(x + 1, y + 1, z)
+    [tl[0], tl[1], br[0], br[1]]
+  end
+
+  def xyz_to_latlon(x, y, z)
+    n = 2 ** z
+    lon_deg = x.to_f / n * 360.0 - 180.0
+    lat_rad = Math.atan(Math.sinh(Math::PI * (1 - 2 * y.to_f / n)))
+    lat_deg = lat_rad * 180.0 / Math::PI
+    return lon_deg, lat_deg
   end
 end
--- owl_viewer/config/routes.rb
+++ owl_viewer/config/routes.rb
@@ -2,67 +2,7 @@ OwlViewer::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-  match 'map'                 => 'changeset#map'
-  match 'changesets.:format'  => 'changeset#changesets'
-  match 'dailymap'            => 'changeset#dailymap'
-  match 'dailymap.:format'    => 'changeset#dailymap'
-  match 'weeklymap'           => 'changeset#weeklymap'
-  match 'weeklymap.:format'   => 'changeset#weeklymap'
-  match 'tile/:id'            => 'changeset#tile', :id => /\d+/
-  match 'tiles/:id'           => 'changeset#tiles', :id => /\d+/
-  match 'tiles/:id.:format'   => 'changeset#tiles', :id => /\d+/
-  match 'feed/:range.:format' => 'changeset#feed', :range => /\d+(-\d+)?(,\d+(-\d+)?)*/
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match 'map'                    => 'changeset#map'
+  match 'changesets.:format'     => 'changeset#changesets'
+  match 'changesets/:zoom/:x/:y' => 'changeset#tile', :zoom => /\d+/, :x => /\d+/, :y => /\d+/
 end
