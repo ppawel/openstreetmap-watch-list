9025bbab7167197064d698d4bed12c07c407e70a
Paweł Paprota <ppawel@fastmail.fm>
2012-11-01 23:51:53 +0100

    Moved tiler logic from plpgsql to Ruby to make it more testable and sane to maintain... Also, the plpgsql code was completely broken anyway.

--- scripts/generate_tiles.rb
+++ scripts/generate_tiles.rb
@@ -1,13 +1,14 @@
 require 'pg'
+require './tiler'
 
 if ARGV.size != 2
   puts "Usage: generate_tiles.rb <zoom level> <limit> [offset]"
   exit
 end
 
-ZOOM_LEVEL = ARGV[0]
-LIMIT = ARGV[1]
-OFFSET = ARGV[2] || 100
+ZOOM_LEVEL = ARGV[0].to_i
+LIMIT = ARGV[1].to_i
+OFFSET = ARGV[2].to_i || 100
 
 $config = {
   'host' => 'localhost',
@@ -19,9 +20,14 @@ $config = {
 
 @conn = PGconn.open(:host => $config['host'], :port => $config['port'], :dbname => $config['dbname'], :user => $config['user'], :password => $config['password'])
 
+tiler = Tiler.new(@conn)
+
 @conn.query("SELECT id FROM changesets ORDER BY created_at DESC LIMIT #{LIMIT}").each do |row|
-  changeset_id = row['id'].to_i
-  puts "Generating tiles for changeset #{changeset_id} at zoom level #{ZOOM_LEVEL}..."
-  tile_count = @conn.query("SELECT OWL_GenerateChangesetTiles(#{changeset_id}, #{ZOOM_LEVEL})").getvalue(0, 0)
-  puts "Done, tile count: #{tile_count}"
+  @conn.transaction do |c|
+    changeset_id = row['id'].to_i
+    puts "Generating tiles for changeset #{changeset_id} at zoom level #{ZOOM_LEVEL}..."
+    tiler.generate(ZOOM_LEVEL, changeset_id)
+  end
+  #tile_count = @conn.query("SELECT OWL_GenerateChangesetTiles(#{changeset_id}, #{ZOOM_LEVEL})").getvalue(0, 0)
+  #puts "Done, tile count: #{tile_count}"
 end
---
+++ scripts/tiler.rb
@@ -0,0 +1,90 @@
+def degrees(rad)
+  rad * 180 / Math::PI
+end
+
+def radians(angle)
+  angle / 180 * Math::PI
+end
+
+# Translated to Ruby rom http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+def latlon2tile(lat_deg, lon_deg, zoom)
+  lat_rad = radians(lat_deg)
+  n = 2.0 ** zoom
+  xtile = ((lon_deg + 180.0) / 360.0 * n).floor
+  ytile = ((1.0 - Math.log(Math.tan(lat_rad) + 1.0 / Math.cos(lat_rad)) / Math::PI) / 2.0 * n).floor
+  return xtile, ytile
+end
+
+# Translated to Ruby rom http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+def tile2latlon(xtile, ytile, zoom)
+  n = 2.0 ** zoom
+  lon_deg = xtile / n * 360.0 - 180.0
+  lat_rad = Math.atan(Math.sinh(Math::PI * (1 - 2 * ytile / n.to_f)))
+  lat_deg = degrees(lat_rad)
+  return lat_deg, lon_deg
+end
+
+class Tiler
+  attr_accessor :conn
+
+  def initialize(conn)
+    @conn = conn
+  end
+
+  def generate(zoom, changeset_id)
+    clear_tiles(changeset_id, zoom)
+    bbox = changeset_bbox(changeset_id)
+    puts bbox
+    tiles = bbox_to_tiles(zoom, bbox)
+
+    tiles.each do |tile|
+      x, y = tile[0], tile[1]
+      lat1, lon1 =  tile2latlon(x, y, zoom)
+      lat2, lon2 =  tile2latlon(x + 1, y + 1, zoom)
+
+      geom = @conn.query("
+        SELECT ST_Intersection(
+          geom::geometry,
+          ST_SetSRID(ST_MakeBox2D(ST_MakePoint(#{lon2}, #{lat1}), ST_MakePoint(#{lon1}, #{lat2})), 4326))
+        FROM changesets WHERE id = #{changeset_id}").getvalue(0, 0)
+
+      if geom != '0107000020E610000000000000' and geom
+        puts 'got tile'
+        @conn.query("INSERT INTO changeset_tiles (changeset_id, zoom, tile_x, tile_y, geom)
+          VALUES (#{changeset_id}, #{zoom}, #{x}, #{y}, '#{geom}')")
+      end
+    end
+
+    puts tiles.inspect
+  end
+
+  protected
+
+  def clear_tiles(changeset_id, zoom)
+    @conn.query("DELETE FROM changeset_tiles WHERE changeset_id = #{changeset_id} AND zoom = #{zoom}")
+  end
+
+  def bbox_to_tiles(zoom, bbox)
+    tiles = []
+    top_left = latlon2tile(bbox['xmin'], bbox['ymin'], zoom)
+    bottom_right = latlon2tile(bbox['xmax'], bbox['ymax'], zoom)
+    min_y = [top_left[1], bottom_right[1]].min
+    max_y = [top_left[1], bottom_right[1]].max
+
+    (top_left[0]..bottom_right[0]).each do |x|
+      (min_y..max_y).each do |y|
+        tiles << [x, y]
+      end
+    end
+
+    tiles
+  end
+
+  def changeset_bbox(changeset_id)
+    result = @conn.query("SELECT ST_XMin(geom::geometry) AS ymin, ST_XMax(geom::geometry) AS ymax,
+      ST_YMin(geom::geometry) AS xmin, ST_YMax(geom::geometry) AS xmax
+      FROM changesets WHERE id = #{changeset_id}")
+    row = result.to_a[0]
+    row.merge(row) {|k, v| v.to_f}
+  end
+end
