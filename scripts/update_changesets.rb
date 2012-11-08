60cb4df4b21ef9895f07864cde8d211de25c025d
Paweł Paprota <ppawel@fastmail.fm>
2012-11-08 23:19:03 +0100

    Added script for updating changesets, simplified PLPGSQL functions.

---
+++ scripts/update_changesets.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+STDOUT.sync = true
+
+require 'pg'
+require 'yaml'
+
+$config = YAML.load_file('../rails/config/database.yml')['development']
+
+@conn = PGconn.open(:host => $config['host'], :port => $config['port'], :dbname => $config['database'],
+  :user => $config['username'], :password => $config['password'])
+
+changesets = @conn.query("SELECT id FROM changesets").to_a
+
+changesets.each_with_index do |row, index|
+  puts "#{Time.now} - Changeset #{row['id']} (#{index + 1} / #{changesets.size})"
+  @conn.query("SELECT OWL_UpdateChangeset(#{row['id']})")
+end
--- sql/owl_functions.sql
+++ sql/owl_functions.sql
@@ -1,54 +1,10 @@
-DROP FUNCTION IF EXISTS OWL_UpdateChangesetGeom(bigint);
-DROP FUNCTION IF EXISTS OWL_UpdateChangesetChangeCount(bigint);
-DROP FUNCTION IF EXISTS OWL_UpdateAllChangesetsGeom();
-DROP FUNCTION IF EXISTS OWL_UpdateAllChangesetsChangeCount();
+DROP FUNCTION IF EXISTS OWL_UpdateChangeset(bigint);
 
-CREATE FUNCTION OWL_UpdateChangesetGeom(bigint) RETURNS void AS $$
-DECLARE
-  changeset_geom geography;
-BEGIN
-
-changeset_geom := (
-  SELECT ST_Collect(DISTINCT g.geom)::geography
-  FROM
-  (
-    SELECT current_geom::geometry AS geom FROM changes WHERE changeset_id = $1
-    UNION
-    SELECT new_geom::geometry AS geom FROM changes WHERE changeset_id = $1
-  ) g);
-
-UPDATE changesets SET geom = changeset_geom WHERE id = $1;
-
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE FUNCTION OWL_UpdateChangesetChangeCount(bigint) RETURNS void AS $$
+CREATE FUNCTION OWL_UpdateChangeset(bigint) RETURNS void AS $$
 DECLARE
   change_count int;
 BEGIN
   change_count := (SELECT COUNT(*) FROM changes WHERE changeset_id = $1);
   UPDATE changesets cs SET num_changes = change_count WHERE cs.id = $1;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE FUNCTION OWL_UpdateAllChangesetsGeom() RETURNS void AS $$
-DECLARE
-  changeset_id bigint;
-BEGIN
-FOR changeset_id IN SELECT id FROM changesets LOOP
-  --RAISE NOTICE '% Changeset %', clock_timestamp(), changeset_id;
-  PERFORM OWL_UpdateChangesetGeom(changeset_id);
-END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE FUNCTION OWL_UpdateAllChangesetsChangeCount() RETURNS void AS $$
-DECLARE
-  changeset_id bigint;
-BEGIN
-FOR changeset_id IN SELECT id FROM changesets LOOP
-  --RAISE NOTICE '% Changeset %', clock_timestamp(), changeset_id;
-  PERFORM OWL_UpdateChangesetChangeCount(changeset_id);
-END LOOP;
 END;
 $$ LANGUAGE plpgsql;
