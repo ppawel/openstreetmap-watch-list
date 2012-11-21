0a1b5bac12081ef91f5cd1db9d19052c8cc9ebbc
Paweł Paprota <ppawel@fastmail.fm>
2012-11-21 13:37:54 +0100

    Removed unused tests.

--- tiler/test/tiler_test.rb
+++ tiler/test/tiler_test.rb
@@ -44,49 +44,6 @@ class TilerTest < Test::Unit::TestCase
     tiler.generate(16, id, prepare_options)
   end
 
-=begin
-  def test_basic_tiling
-    setup_db
-    exec_sql_file('test_data.sql')
-    tiler = Tiler::Tiler.new(@conn)
-    count = tiler.generate(16, 13517262, prepare_options)
-    assert_equal(62, count)
-  end
-
-  def test_invalid_geometry
-    setup_db
-    exec_sql_file('test_changeset_13440045.sql')
-    tiler = Tiler::Tiler.new(@conn)
-    count = tiler.generate(16, 13440045, prepare_options)
-    assert_equal(14, count)
-  end
-
-  # This checks for simple tiling stuff plus a rounding issue that occurred on zark with geography type.
-  def test_simple_way
-    setup_db
-    exec_sql_file('test_simple_way.sql')
-    tiler = Tiler::Tiler.new(@conn)
-    count = tiler.generate(16, 13440045, prepare_options)
-    assert_equal(5, count)
-
-    # Gather points to check if they are represented in tiles after tiling.
-    points = []
-    for point in @conn.query('SELECT ST_AsText((g.dump).geom) FROM (SELECT ST_DumpPoints(current_geom) dump FROM
-        changes) g').to_a
-      points << point['st_astext'].gsub('POINT(', '').gsub(')', '')
-    end
-
-    # Now check that tile geometry contains sane data (without changed coordinates).
-    geom_string = @conn.query('SELECT ST_AsText(geom) FROM changeset_tiles').to_a.reduce('') {|total, row| total + row['st_astext']}
-
-    assert_equal(false, geom_string.include?('999999'))
-    assert_equal(false, geom_string.include?('5180885999359'))
-
-    for point in points
-      assert_equal(true, geom_string.include?(point))
-    end
-  end
-=end
   def prepare_options
     options = {}
     options[:changesets] ||= ['all']
