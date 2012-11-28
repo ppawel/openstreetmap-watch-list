e865040fe87a3e475ad7788d825ca2f162024b81
Paweł Paprota <ppawel@fastmail.fm>
2012-11-28 14:18:32 +0100

    #17 Now download_changesets.rb is suitable for starting from crontab.

--- scripts/download_changesets.rb
+++ scripts/download_changesets.rb
@@ -16,6 +16,20 @@ def parse_tags(changeset_el)
   tags
 end
 
+def update_changeset(changeset_el)
+  id = changeset_el['id'].to_i
+  count = @conn.exec("
+    UPDATE changesets
+    SET
+      closed_at = #{!changeset_el['closed_at'] ? 'NULL' : '\'' + changeset_el['closed_at'] + '\''},
+      tags = '#{PGconn.escape(parse_tags(changeset_el).to_s.gsub('{', '').gsub('}', ''))}'::hstore
+    WHERE id = #{id}").cmd_tuples
+end
+
+def save_state(state)
+  File.open('state.yaml', 'w') {|f| f.puts(state.to_yaml)}
+end
+
 $config = YAML.load_file('../rails/config/database.yml')['development']
 current_state = YAML.load_file('state.yaml')
 remote_state = YAML.load(open('http://planet.openstreetmap.org/replication/changesets/state.yaml').read)
@@ -23,29 +37,36 @@ remote_state = YAML.load(open('http://planet.openstreetmap.org/replication/chang
 @conn = PGconn.open(:host => $config['host'], :port => $config['port'], :dbname => $config['database'],
   :user => $config['username'], :password => $config['password'])
 
-@conn.transaction do |c|
-  for id in (current_state['sequence'].to_i..remote_state['sequence'].to_i)
-    file_id = id.to_s.rjust(9, '0')
-    puts "file_id = #{file_id}"
-    open("http://planet.openstreetmap.org/replication/changesets/#{file_id[0..2]}/#{file_id[3..5]}/#{file_id[6..8]}.osm.gz") do |f|
-      gz = Zlib::GzipReader.new(f)
-      text = gz.read
-      puts text
-      xml = Nokogiri::XML(text)
-      xml.root.children.each do |changeset_el|
-        next unless changeset_el.element?
-        id = changeset_el['id'].to_i
-        print "Processing changeset #{id}... "
-        print @conn.exec("
-          UPDATE changesets
-          SET
-            closed_at = #{!changeset_el['closed_at'] ? 'NULL' : '\'' + changeset_el['closed_at'] + '\''},
-            tags = '#{PGconn.escape(parse_tags(changeset_el).to_s.gsub('{', '').gsub('}', ''))}'::hstore
-          WHERE id = #{id}").cmd_tuples
-        puts
+for id in (current_state['sequence'].to_i + 1..remote_state['sequence'].to_i)
+  file_id = id.to_s.rjust(9, '0')
+  puts "file_id = #{file_id}"
+
+  begin
+    @conn.transaction do |c|
+      open("http://planet.openstreetmap.org/replication/changesets/#{file_id[0..2]}/#{file_id[3..5]}/#{file_id[6..8]}.osm.gz") do |f|
+        gz = Zlib::GzipReader.new(f)
+        text = gz.read
+        puts text
+        xml = Nokogiri::XML(text)
+        xml.root.children.each do |changeset_el|
+          next unless changeset_el.element?
+          changeset_id = changeset_el['id'].to_i
+          print "Processing changeset #{changeset_id}... "
+          count = update_changeset(changeset_el)
+          print count
+          puts
+
+          if count == 0 and changeset_el['open'] == 'false'
+            puts ' Not in the database, stopping...'
+            raise
+          end
+        end
       end
     end
+
+    current_state['sequence'] = id
+    save_state(current_state)
+  rescue
+    exit
   end
 end
-
-File.open('state.yaml', 'w') {|f| f.puts(remote_state.to_yaml)}
