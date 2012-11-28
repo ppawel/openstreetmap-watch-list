27864dd261e351acf9da19d5d6a53e6f78e9a15d
Paweł Paprota <ppawel@fastmail.fm>
2012-11-28 15:00:08 +0100

    Replication scripts.

--- scripts/replicate_changesets.sh
+++ scripts/replicate_changesets.sh
@@ -4,12 +4,14 @@
 PATH=/home/ppawel/jdk/bin:/home/ppawel/bin:/home/ppawel/.gem/ruby/1.9.1/bin/:$PATH
 export PATH
 
-cd /home/ppawel/replication
+DIR=$(dirname $0)
+cd $DIRcd /home/ppawel/replication
 
 set -e
 
 (
   # Try to lock on the lock file (fd 200)
   flock -x -n 200
-  osmosis -v --rri --lpc --write-owldb-change authFile=~/authFile invalidActionsMode=LOG &>> ~/replication.log
-) 200>/home/ppawel/replication/lock
+  ./download_changesets.rb &>> ~/changesets.log
+) 200>/home/ppawel/.changesets.lock
+
