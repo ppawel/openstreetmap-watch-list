79de3c6f47b5a94e1cdf9a54250b15909b7c92f8
Paweł Paprota <ppawel@fastmail.fm>
2012-12-12 20:08:06 +0100

    Cleaned up Rails configuration in preparation for Rails tests.

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -50,6 +50,8 @@ module OwlViewer
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.active_record.schema_format = :sql
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
