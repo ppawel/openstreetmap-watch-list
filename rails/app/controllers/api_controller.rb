408ad40cbc423a4e84bf8f010a2d2fbafce558d8
Paweł Paprota <ppawel@fastmail.fm>
2012-12-12 18:50:19 +0100

    Returning change geometry as separate features.

--- rails/app/controllers/api_controller.rb
+++ rails/app/controllers/api_controller.rb
@@ -49,7 +49,7 @@ private
   def find_changesets_by_tile(format)
     @x, @y, @zoom = get_xyz(params)
     Changeset.find_by_sql("
-      SELECT cs.* #{format == 'geojson' ? ', ST_AsGeoJSON(t.geom) AS geojson' : ''}
+      SELECT cs.* #{format == 'geojson' ? ', (SELECT array_agg(ST_AsGeoJSON(g)) FROM unnest(t.geom) AS g) AS geojson' : ''}
         #{format != 'atom' ? ', (SELECT ST_Extent(x.geom) FROM (SELECT unnest(t.geom)::box2d AS geom) x)::text AS tile_bbox, cs.bbox::box2d::text AS total_bbox' : ''}
       FROM tiles t
       INNER JOIN changesets cs ON (cs.id = t.changeset_id)
@@ -120,14 +120,27 @@ private
 
   def changesets_to_geojson(changesets, x, y, zoom)
     geojson = { "type" => "FeatureCollection", "features" => []}
-    changesets.each do |changeset|
-      feature = { "type" => "Feature",
-        "id" => "#{changeset.id}_#{x}_#{y}_#{zoom}}",
-        "geometry" => changeset.geojson ? JSON[changeset.geojson] : nil,
-        "properties" => changeset.as_json
-      }
-      geojson['features'] << feature
+    for changeset in changesets
+      changeset_geojson = {"type" => "FeatureCollection", "properties" => changeset.as_json, "features" => []}
+      if changeset.geojson
+        for change_geojson in pg_string_to_array(changeset.geojson)
+          next if change_geojson.nil?
+          feature = {"type" => "Feature", "id" => "#{changeset.id}_#{x}_#{y}_#{zoom}}"}
+          feature['geometry'] = JSON[change_geojson] if change_geojson
+          changeset_geojson['features'] << feature
+        end
+      end
+      geojson['features'] << changeset_geojson
     end
     geojson
+  end
+
+  def pg_string_to_array(str)
+    dup = str.dup
+    dup[0] = '['
+    dup[-1] = ']'
+    dup.gsub!('NULL', 'nil')
+    puts dup
+    eval(dup)
   end
 end
