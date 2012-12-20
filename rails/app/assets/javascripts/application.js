3cd5946f23626ab6f5db204de4383a690ab03fef
Paweł Paprota <ppawel@fastmail.fm>
2012-12-20 18:19:24 +0100

    Removed inclusion of jQuery, Leaflet and other JS libraries - this is not needed for OWL API.

--- rails/app/assets/javascripts/application.js
+++ rails/app/assets/javascripts/application.js
@@ -10,10 +10,4 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery.scrollTo-min
-//= require jsrender
-//= require leaflet-src
-//= require TileLayer.GeoJSON
 //= require_tree .
