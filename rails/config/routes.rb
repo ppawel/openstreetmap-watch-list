df03d82e1a2fae9f83d0bbe96b9806f7d5e379d5
Paweł Paprota <ppawel@fastmail.fm>
2012-12-20 18:47:54 +0100

    Added api/0.1/ prefix to all routes to make the API versionable.

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -6,15 +6,15 @@ OwlViewer::Application.routes.draw do
   # first created -> highest priority.
 
   # Tile operations
-  match 'changesets/:zoom/:x/:y.atom' => 'api#changesets_tile_atom', :constraints => @xyz_constrains, :format => 'atom'
-  match 'changesets/:zoom/:x/:y.geojson' => 'api#changesets_tile_geojson', :constraints => @xyz_constrains, :format => 'json'
-  match 'changesets/:zoom/:x/:y.json' => 'api#changesets_tile_json', :constraints => @xyz_constrains, :format => 'json'
+  match 'api/0.1/changesets/:zoom/:x/:y.atom' => 'api#changesets_tile_atom', :constraints => @xyz_constrains, :format => 'atom'
+  match 'api/0.1/changesets/:zoom/:x/:y.geojson' => 'api#changesets_tile_geojson', :constraints => @xyz_constrains, :format => 'json'
+  match 'api/0.1/changesets/:zoom/:x/:y.json' => 'api#changesets_tile_json', :constraints => @xyz_constrains, :format => 'json'
 
   # Tile range operations
-  match 'changesets/:zoom/:x1/:y1/:x2/:y2.atom' => 'api#changesets_tilerange_atom', :constraints => @range_constrains, :format => 'atom'
-  match 'changesets/:zoom/:x1/:y1/:x2/:y2.geojson' => 'api#changesets_tilerange_geojson', :constraints => @range_constrains, :format => 'json'
-  match 'changesets/:zoom/:x1/:y1/:x2/:y2.json' => 'api#changesets_tilerange_json', :constraints => @range_constrains, :format => 'json'
+  match 'api/0.1/changesets/:zoom/:x1/:y1/:x2/:y2.atom' => 'api#changesets_tilerange_atom', :constraints => @range_constrains, :format => 'atom'
+  match 'api/0.1/changesets/:zoom/:x1/:y1/:x2/:y2.geojson' => 'api#changesets_tilerange_geojson', :constraints => @range_constrains, :format => 'json'
+  match 'api/0.1/changesets/:zoom/:x1/:y1/:x2/:y2.json' => 'api#changesets_tilerange_json', :constraints => @range_constrains, :format => 'json'
 
-  match 'summary/:zoom/:x/:y' => 'api#summary_tile', :constraints => @xyz_constrains
-  match 'summary/:zoom/:x1/:y1/:x2/:y2' => 'api#summary_tilerange', :constraints => @range_constrains, :format => 'json'
+  match 'api/0.1/summary/:zoom/:x/:y' => 'api#summary_tile', :constraints => @xyz_constrains
+  match 'api/0.1/summary/:zoom/:x1/:y1/:x2/:y2' => 'api#summary_tilerange', :constraints => @range_constrains, :format => 'json'
 end
