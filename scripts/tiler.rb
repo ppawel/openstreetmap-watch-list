4f7af8d235138e45f5ea8148888f35ab9b2f6e15
Paweł Paprota <ppawel@fastmail.fm>
2012-12-26 17:47:20 +0100

    Added support for parallel processing.

--- scripts/tiler.rb
+++ scripts/tiler.rb
@@ -10,6 +10,7 @@ require 'yaml'
 
 require 'cmdline_options'
 require 'logging'
+require 'parallel'
 require 'tiler'
 
 $config = YAML.load_file('../rails/config/database.yml')['development']
@@ -18,21 +19,23 @@ options = Tiler::parse_cmdline_options
 
 puts options.inspect
 
-@conn = PGconn.open(:host => $config['host'], :port => $config['port'], :dbname => $config['database'],
-  :user => $config['username'], :password => $config['password'])
-
-tiler = Tiler::Tiler.new(@conn)
-
 zoom = 16
-count = 0
 
-ARGF.each_line do |line|
-  changeset_id = line.to_i
+changeset_ids = ARGF.each_line.collect {|line| line.to_i}
+@conn = nil
+
+Parallel.each_with_index(changeset_ids, :in_processes => 3) do |changeset_id, count|
   next if changeset_id == 0
-  count += 1
+
+  if !@conn
+    @conn = PGconn.open(:host => $config['host'], :port => $config['port'], :dbname => $config['database'],
+      :user => $config['username'], :password => $config['password'])
+    @tiler = Tiler::Tiler.new(@conn)
+  end
+
   before = Time.now
   puts "Generating tiles for changeset #{changeset_id}... (#{count})"
-  tile_count = tiler.generate(zoom, changeset_id, options)
+  tile_count = @tiler.generate(zoom, changeset_id, options)
   puts "Done, tile count: #{tile_count}"
   puts "Changeset #{changeset_id} took #{Time.now - before}s (#{count})"
 end
