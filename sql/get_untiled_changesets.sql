420e6b5772f99ea4508aa2d28c451057b70cc990
Paweł Paprota <ppawel@fastmail.fm>
2012-12-30 10:59:20 +0100

    Added SQL script for untiled changesets.

--- sql/get_untiled_changesets.sql
+++ sql/get_untiled_changesets.sql
@@ -1,6 +1,18 @@
-SELECT DISTINCT n.changeset_id
-FROM nodes n
-WHERE n.tstamp >= NOW() - INTERVAL '1 hour'
+SELECT changeset_id
+FROM
+
+  (SELECT changeset_id, MAX(tstamp) AS tstamp
+  FROM nodes n
+  WHERE n.tstamp >= NOW() - INTERVAL '12 hour'
+  GROUP BY changeset_id
+
+  UNION
+
+  SELECT changeset_id, MAX(tstamp) AS tstamp
+  FROM ways w
+  WHERE w.tstamp >= NOW() - INTERVAL '12 hour'
+  GROUP BY changeset_id) x
+
 GROUP BY changeset_id
-HAVING (NOT EXISTS (SELECT 1 FROM tiles WHERE changeset_id = n.changeset_id) OR
-  MAX(n.tstamp) < (SELECT MAX(tstamp) FROM tiles WHERE changeset_id = n.changeset_id))
+HAVING (NOT EXISTS (SELECT 1 FROM tiles WHERE changeset_id = x.changeset_id) OR
+  MAX(x.tstamp) < (SELECT MAX(tstamp) FROM tiles WHERE changeset_id = x.changeset_id))
