5b6881d6d77b3761048644d2f0aa9d36ad260261
Paweł Paprota <ppawel@fastmail.fm>
2013-02-08 19:24:09 +0100

    Updated the query for Kothic tiles, added nodes.

--- rails/app/controllers/map_api_controller.rb
+++ rails/app/controllers/map_api_controller.rb
@@ -16,21 +16,29 @@ class MapApiController < ApplicationController
         SELECT ST_Transform(ST_SetSRID('LINESTRING(#{tile_bbox[0]} #{tile_bbox[1]},
           #{tile_bbox[2]} #{tile_bbox[3]})'::geometry, 4326), 900913)::box2d g
       )
-      SELECT ST_AsGeoJSON(ST_TransScale(ST_Transform(geom, 900913),
-        (SELECT -ST_XMax(g) FROM tile_bbox),
-        (SELECT -ST_YMax(g) FROM tile_bbox),
-        (SELECT 10000 / (ST_XMin(g) - ST_XMax(g)) FROM tile_bbox),
-        (SELECT 10000 / (ST_YMin(g) - ST_YMax(g)) FROM tile_bbox)
-
-         ), 0
-
-      ) AS geojson,
+      (SELECT DISTINCT ON (w.id, t.x, t.y) ST_AsGeoJSON(ST_TransScale(ST_Transform(geom, 900913),
+        (SELECT -ST_XMin(g) FROM tile_bbox),
+        (SELECT -ST_YMin(g) FROM tile_bbox),
+        (SELECT 10000 / (ST_XMax(g) - ST_XMin(g)) FROM tile_bbox),
+        (SELECT 10000 / (ST_YMax(g) - ST_YMin(g)) FROM tile_bbox)), 0) AS geojson,
         w.tags
       FROM way_tiles t
       INNER JOIN ways w ON (w.id = t.way_id AND w.version = t.version)
       WHERE x >= #{@x * (2 ** (16 - @zoom))} AND y >= #{@y * (2 ** (16 - @zoom))} AND
         x <= #{(@x + 1) * (2 ** (16 - @zoom))} AND y <= #{(@y + 1) * (2 ** (16 - @zoom))}
-
+      ORDER BY w.id, t.x, t.y, t.rev DESC)
+        UNION
+      (SELECT DISTINCT ON (n.id) ST_AsGeoJSON(ST_TransScale(ST_Transform(n.geom, 900913),
+        (SELECT -ST_XMin(g) FROM tile_bbox),
+        (SELECT -ST_YMin(g) FROM tile_bbox),
+        (SELECT 10000 / (ST_XMax(g) - ST_XMin(g)) FROM tile_bbox),
+        (SELECT 10000 / (ST_YMax(g) - ST_YMin(g)) FROM tile_bbox)), 0) AS geojson,
+        n.tags
+      FROM nodes n
+      WHERE geom && (SELECT ST_Transform(ST_Setsrid(g, 900913), 4326) FROM tile_bbox)
+        AND n.tags != ''::hstore
+      ORDER BY n.id, n.version DESC)
+      LIMIT 10000
       ").to_a
 
     geojson = []
