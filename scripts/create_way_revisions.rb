7cd8c9942a3329c857dd749026502bd8faca6708
Paweł Paprota <ppawel@fastmail.fm>
2013-02-17 09:13:15 +0100

    More handy helper script for creating way revisions.

--- scripts/create_way_revisions.rb
+++ scripts/create_way_revisions.rb
@@ -1,20 +1,17 @@
 #!/usr/bin/env ruby
 
-STDOUT.sync = true
-
 require 'pg'
 require 'yaml'
-
-LIMIT = 10000
 
 $config = YAML.load_file('../rails/config/database.yml')['development']
 
 @conn = PGconn.open(:host => $config['host'], :port => $config['port'], :dbname => $config['database'],
   :user => $config['username'], :password => $config['password'])
+@conn.prepare('create', 'SELECT OWL_CreateWayRevisions($1)')
 
-i = 0
-while true do
-  @conn.exec("SELECT OWL_CreateWayRevisions(id) FROM ways ORDER BY id DESC LIMIT #{LIMIT} OFFSET #{i * LIMIT} ")
-  i += 1
-  puts "---- #{LIMIT * i}"
+way_ids = ARGF.each_line.collect {|line| line.to_i}
+
+way_ids.each_with_index do |way_id, count|
+  @conn.exec_prepared('create', [way_id])
+  puts "#{way_id} (#{count} / #{way_ids.size})"
 end
