59508d75122746f67b135177a557feb678a957a7
Paweł Paprota <ppawel@fastmail.fm>
2013-02-18 21:34:31 +0100

    Fixed dump script.

--- scripts/dump_way_revisions.rb
+++ scripts/dump_way_revisions.rb
@@ -9,7 +9,7 @@ $config = YAML.load_file('../rails/config/database.yml')['development']
   :user => $config['username'], :password => $config['password'])
 
 ARGF.each_line do |way_id|
-  @conn.exec("COPY (SELECT * FROM OWL_CreateWayRevisions(#{way_id}, true)) TO STDOUT")
+  @conn.exec("COPY (SELECT * FROM OWL_GenerateWayRevisions(#{way_id}, true)) TO STDOUT")
   while not (data = @conn.get_copy_data).nil?
     puts data
   end
